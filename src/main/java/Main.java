918a7662cf633547167dfef813248bc5cc8c26ca
Fabricio Quagliariello <matias.quaglia@gmail.com>
2013-11-19 09:03:04 -0300

    added some test routes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
 import static spark.Spark.*;
+
 import spark.*;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        staticFileLocation("/public");
+
         get(new Route("/") {
             @Override
             public Object handle(Request request, Response response) {
                 return "Hello World!";
+            }
+        });
+
+        get(new Route("/hello/:name") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Hello " + request.params(":name");
+            }
+        });
+
+        get(new Route("/say/*/to/*") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "Listen, " + request.splat()[1] + ": " + request.splat()[0] + "!";
             }
         });
     }
